e1f4693df1c0a06d8d607e543fecbaab0cc8bd2f
1xo2 <yanivsfreelance@hotmail.com>
2021-01-09 17:52:16 +0200

    mediaQuery bool

--- src/@lekoarts/gatsby-theme-cara/templates/cara.tsx
+++ src/@lekoarts/gatsby-theme-cara/templates/cara.tsx
@@ -7,17 +7,37 @@ import Projects2 from "../components/projects2"
 import About from "../../../../node_modules/@lekoarts/gatsby-theme-cara/src/components/about"
 import Contact from "../../../../node_modules/@lekoarts/gatsby-theme-cara/src/components/contact"
 
+
+const isMobile = window.matchMedia("(max-width: 400px)").matches;
+
 const Cara = () => (
   <Layout>
-    <Parallax pages={7}>
+    <Parallax pages={isMobile? 8:7}>
       <Hero offset={0} factor={1} />
       <Projects offset={1} factor={2} />
-      <Projects2 offset={3} factor={2} />
-      <About offset={5} factor={1} />
-      <Contact offset={6} factor={1} />
+
+      <Projects2 offset={isMobile? 3.5:3} factor={isMobile? 3.5:2} />
+
+      <About offset={isMobile? 6.6:5} factor={1} />
+      <Contact offset={isMobile? 7:6} factor={1} />
     </Parallax>
   </Layout>
 )
+// const Cara = () => (
+//   <Layout>
+//     <Parallax pages={7}>
+//       <Hero offset={0} factor={1} />
+//       <Projects offset={1} factor={2} />
+
+//       <Projects2 offset={3} factor={2} />
+
+//       <About offset={5} factor={1} />
+//       <Contact offset={6} factor={1} />
+//     </Parallax>
+//   </Layout>
+// )
+
+
 // const Cara = () => (
 //   <Layout>
 //     <Parallax pages={5}>
