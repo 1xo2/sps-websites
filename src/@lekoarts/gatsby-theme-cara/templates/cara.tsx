7f45dd8e5d84286e03a81359d71f394f5f058667
1xo2 <yanivsfreelance@hotmail.com>
2021-01-09 21:00:00 +0200

    ok

--- src/@lekoarts/gatsby-theme-cara/templates/cara.tsx
+++ src/@lekoarts/gatsby-theme-cara/templates/cara.tsx
@@ -1,28 +1,29 @@
-import React from "react"
-import { Parallax } from "react-spring/renderprops-addons.cjs"
-import Layout from "../../../../node_modules/@lekoarts/gatsby-theme-cara/src/components/layout"
-import Hero from "../../../../node_modules/@lekoarts/gatsby-theme-cara/src/components/hero"
-import Projects from "../../../../node_modules/@lekoarts/gatsby-theme-cara/src/components/projects"
-import Projects2 from "../components/projects2"
-import About from "../../../../node_modules/@lekoarts/gatsby-theme-cara/src/components/about"
-import Contact from "../../../../node_modules/@lekoarts/gatsby-theme-cara/src/components/contact"
+import React from "react";
+import { Parallax } from "react-spring/renderprops-addons.cjs";
+import Layout from "../../../../node_modules/@lekoarts/gatsby-theme-cara/src/components/layout";
+import Hero from "../../../../node_modules/@lekoarts/gatsby-theme-cara/src/components/hero";
+import Projects from "../../../../node_modules/@lekoarts/gatsby-theme-cara/src/components/projects";
+import Projects2 from "../components/projects2";
+import About from "../../../../node_modules/@lekoarts/gatsby-theme-cara/src/components/about";
+import Contact from "../../../../node_modules/@lekoarts/gatsby-theme-cara/src/components/contact";
 
-
-const isMobile = window.matchMedia("(max-width: 400px)").matches;
+let isMobile = false;
+if (typeof window !== `undefined`)
+  isMobile = window.matchMedia("(max-width: 400px)").matches;
 
 const Cara = () => (
   <Layout>
-    <Parallax pages={isMobile? 8:7}>
+    <Parallax pages={isMobile ? 8 : 7}>
       <Hero offset={0} factor={1} />
       <Projects offset={1} factor={2} />
 
-      <Projects2 offset={isMobile? 3.5:3} factor={isMobile? 3.5:2} />
+      <Projects2 offset={isMobile ? 3.5 : 3} factor={isMobile ? 3.5 : 2} />
 
-      <About offset={isMobile? 6.6:5} factor={1} />
-      <Contact offset={isMobile? 7:6} factor={1} />
+      <About offset={isMobile ? 6.6 : 5} factor={1} />
+      <Contact offset={isMobile ? 7 : 6} factor={1} />
     </Parallax>
   </Layout>
-)  
+);
 
 // const Cara = () => (
 //   <Layout>
@@ -38,7 +39,6 @@ const Cara = () => (
 //   </Layout>
 // )
 
-
 // const Cara = () => (
 //   <Layout>
 //     <Parallax pages={5}>
@@ -50,4 +50,4 @@ const Cara = () => (
 //   </Layout>
 // )
 
-export default Cara
+export default Cara;
